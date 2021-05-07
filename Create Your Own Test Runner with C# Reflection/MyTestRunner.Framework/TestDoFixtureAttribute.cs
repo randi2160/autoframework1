2621ea96f016d007734cb596331754bd7176204e
randi2160 <randi2160@yahoo.com>
2021-05-07 08:58:31 -0500

    commmit

---
+++ Create Your Own Test Runner with C# Reflection/MyTestRunner.Framework/TestDoFixtureAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestRunner.Tests
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TestDoFixtureAttribute : Attribute
+    {
+        public TestDoFixtureAttribute(string name) => this.Name = name;
+        public string Name { get; }
+    }
+
+   
+    
+}
---
+++ Create Your Own Test Runner with C# Reflection/MyTestRunner.Tests/WhenCar.cs
@@ -0,0 +1,49 @@
+﻿namespace MyTestRunner.Tests
+{
+    using Framework;
+    using Shouldly;
+
+    [Subject("Car")]
+    public class WhenCarIsStarted
+    {
+        const string Model = "BMW";
+
+        static Car car;
+
+        Given context = () =>
+        {
+            car = new Car();
+            car.Produce(Model);
+        };
+
+        Because of = () => car.Start();
+
+        It shouldBeRunning = () => car.IsRunning.ShouldBe(true);
+
+        It shouldHaveCorrectModel = () => car.Model.ShouldBe(Model);
+    }
+
+    [Subject("Car")]
+    public class WhenCarIsStopped
+    {
+        static Car car;
+
+        Given context = () => car = new Car();
+
+        Because of = () => car.Stop();
+
+        It shouldBeRunning = () => car.IsRunning.ShouldBe(false);
+    }
+
+    [Subject("SOMETHINGELSE")]
+    public class WhenSomethingElse
+    {
+        static Car van;
+
+        Given context = () => van = new Car();
+
+        Because of = () => van.Stop();
+
+        It shouldBeRunning = () => van.IsRunning.ShouldBe(false);
+    }
+}
